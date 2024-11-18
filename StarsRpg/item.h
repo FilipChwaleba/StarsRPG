45fd212a9a6567862797f836b7a0ff9b20942dd9
FilipChwaleba <152343853+FilipChwaleba@users.noreply.github.com>
2024-11-19 00:02:21 +0100

    Update item.h

--- StarsRpg/item.h
+++ StarsRpg/item.h
@@ -8,7 +8,9 @@ class ItemDelegate {
 public:
 	std::string Name;
 	virtual const char* GetType() = 0;
+	
 protected:
+	
 	ItemDelegate(std::string name) : Name(name){}
 };
 
@@ -45,14 +47,6 @@ private:
 
 	friend class ItemManager;
 };
-
-
-
-
-
-
-
-
 
 
 //=====================================================
@@ -115,6 +109,7 @@ private:
 
 
 //use this one in runtime
+#include <iostream> //for outputing tests
 class Item {
 public:
 	const ItemDelegate* GetData() { return _data; }
@@ -125,11 +120,38 @@ public:
 		}
 		
 	}
+	const bool getMarkedForDeletion() const{ return marked_for_deletion; } 
 private:
 	ItemDelegate* _data;
+	bool marked_for_deletion = false; 
 	Item(ItemDelegate* item) : _data(item) {}
 	friend class ItemManager;
-	friend class PlayerCharacter; //quickFix
+	friend class PlayerCharacter;
 	
+	friend std::ostream& operator<<(std::ostream& os, const Item& t) {
+		Armor* tmp_cast = dynamic_cast<Armor*>(t._data);
+		if (tmp_cast) {
+			return os << tmp_cast->Name 
+				<< "(Atk: "  << tmp_cast->Stats.Attack
+				<< ", Def: " << tmp_cast->Stats.Defence
+				<< ", Atn: " << tmp_cast->Stats.Attunment
+				<< ", Wrd: " << tmp_cast->Stats.Warding
+				<< ", Spd: " << tmp_cast->Stats.Speed << ")\n";
+		}
+		Weapon* tmp_cast2 = dynamic_cast<Weapon*>(t._data);
+		if (tmp_cast2) {
+			return os << tmp_cast2->Name << '(' << tmp_cast2->MinDamage << '/' << tmp_cast2->MaxDamage << ')'
+				<< "(Atk: " <<  tmp_cast2->Stats.Attack
+				<< ", Def: " << tmp_cast2->Stats.Defence
+				<< ", Atn: " << tmp_cast2->Stats.Attunment
+				<< ", Wrd: " << tmp_cast2->Stats.Warding
+				<< ", Spd: " << tmp_cast2->Stats.Speed << ")\n";
+		}
+		Consumable* tmp_cast3 = dynamic_cast<Consumable*>(t._data);
+		if (tmp_cast3) {
+			return os << tmp_cast3->Name << '(' << tmp_cast3->Quantity << ")\n";	
+		}
+		return os; 
+	}
 };
 
